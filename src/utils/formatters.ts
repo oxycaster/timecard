de3114b8c42aa6932e59ce6d256397cc163e326e
oxycaster <>
2025-03-22 05:17:04 +0900

    現在時刻を秒まで表示するように修正

--- src/utils/formatters.ts
+++ src/utils/formatters.ts
@@ -2,6 +2,7 @@ export const formatTime = (date: Date): string => {
   return date.toLocaleTimeString('ja-JP', {
     hour: '2-digit',
     minute: '2-digit',
+    second: '2-digit',
     hour12: false
   });
 };
